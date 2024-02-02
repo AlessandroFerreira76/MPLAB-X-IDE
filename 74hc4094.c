cf7dc706d3e7da2a1aa62332ac4a6795a5eed11c
Alessandro F <34175788+tudoale@users.noreply.github.com>
2024-02-02 12:41:45 -0300

    Add files via upload
    BIBLIOTECAS 74HC595

---
+++ 74hc4094.c
@@ -0,0 +1,28 @@
+/*
+ * File:   74hc4094.c
+ * Author: Fernando Martins, John Santos
+ *
+ * Created on June 21, 2023, 9:39 AM
+ */
+
+#include "74hc4094.h"
+
+void write74hc4094(uint8_t* _dataOut) {
+    
+    uint8_t j;
+
+    for (j = 0; j < 8 ; j++) { // 2 Shifters, 8 bit per shifter
+        //4094 - data
+        PORTB = (((uint8_t)*_dataOut >> j) & 0b1) ? (PORTB |= _BV(_PORTB_RB4_POSN)) : (PORTB &= ~_BV(_PORTB_RB4_POSN));
+        //4094 - Clock
+        PORTB |= _BV(_PORTB_RB2_POSN);
+        NOP();
+        PORTB &= ~_BV(_PORTB_RB2_POSN);
+    }
+    //4094 - Strobe
+    PORTB |= _BV(_PORTB_RB3_POSN);
+    NOP();
+    PORTB &= ~_BV(_PORTB_RB3_POSN);
+    
+    return;
+}
