4a817614d6238683b045972f9070712eb1db8693
Alessandro F. <34175788+tudoale@users.noreply.github.com>
2024-08-24 22:03:35 -0300

    First commit from PIC16F1779

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=BLINK_16F1779.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BLINK_16F1779.X.production.hex
